f1e996a50a74fd78b9d002d14bc46350b5bbff22
Haffi <hafthort12@ru.is>
2015-04-01 21:31:49 +0000

    added pipe images and some changes to pipe js

--- app/scripts/pipes.js
+++ app/scripts/pipes.js
@@ -1,33 +1,43 @@
-// window.Pipes = (function() {
-// 	'use strict';
-
-// 	var Pipes = function(el, game) {
-		
-// 	};
-
-// 	/**
-// 	 * Resets the state of the player for a new game.
-// 	 */
-// 	Pipes.prototype.reset = function() {
-		
-// 	};
-
-// 	Pipes.prototype.onFrame = function(delta) {
-
-// 		// Update UI
-// 		// this.el.css('transform', 'translateZ(0) translate(' + this.pos.x + 'em, ' + this.pos.y + 'em)');
-// 	};
+window.Pipes = (function() {
+	'use strict';
 
 
-// 	Pipes.prototype.checkCollision = function() {
-// 		// if (this.pos.x < 0 ||
-// 		// 	this.pos.x + WIDTH > this.game.WORLD_WIDTH ||
-// 		// 	this.pos.y < 0 ||
-// 		// 	this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
-// 		// 	return this.game.gameover();
-// 		// }
-// 	};
+	var INITIAL_POSITION_X = 102.4;
+	var INITIAL_POSITION_Y = 10;
 
-// 	return Pipes;
+	var Pipes = function(el, game) {
+		// this.el = el;
+		this.el = $('<div class="Pipes"></div>');
+		this.game = game;
+		this.pos = { x: 0, y: 0 };
+	};
 
-// })();
+	/**
+	 * Resets the state of the player for a new game.
+	 */
+	Pipes.prototype.reset = function() {
+		this.pos.x = INITIAL_POSITION_X;
+		this.pos.y = INITIAL_POSITION_Y;
+	};
+
+	Pipes.prototype.onFrame = function(delta) {
+
+		delta++;
+
+		// Update UI
+		this.el.css('transform', 'translate3d(' + this.pos.x + 'em, ' + this.pos.y + 'em, 0)');
+	};
+
+
+	Pipes.prototype.checkCollision = function() {
+		// if (this.pos.x < 0 ||
+		// 	this.pos.x + WIDTH > this.game.WORLD_WIDTH ||
+		// 	this.pos.y < 0 ||
+		// 	this.pos.y + HEIGHT > this.game.WORLD_HEIGHT) {
+		// 	return this.game.gameover();
+		// }
+	};
+
+	return Pipes;
+
+})();
